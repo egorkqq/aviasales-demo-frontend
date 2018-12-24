38dfdc256540671db871d0e7967b0e95f960e10b
egorkqq <egorkqq@gmail.com>
2018-12-24 22:16:53 +0500

    feat: add images, icons and their imports

---
+++ src/components/Home/PopularWays/Categories/icons/index.js
@@ -0,0 +1,8 @@
+import category1 from "./svg-1.svg";
+import category2 from "./svg-2.svg";
+import category3 from "./svg-3.svg";
+import category4 from "./svg-4.svg";
+import category5 from "./svg-5.svg";
+import category0 from "./svg-0.svg";
+
+export { category0, category1, category2, category3, category4, category5 };
---
+++ src/components/Home/PopularWays/CityCards/images/index.js
@@ -0,0 +1,7 @@
+import min from "./min (2).jpg";
+import barca from "./barca (2).jpg";
+import krasnodar from "./krasnodar@2x.jpg";
+import simf from "./simf (2).jpg";
+import sochi from "./sochi@2x.jpg";
+import spb from "./spb (2).jpg";
+export { min, barca, krasnodar, simf, sochi, spb };
